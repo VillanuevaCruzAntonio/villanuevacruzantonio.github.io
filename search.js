d0e8775f8e63e95280ceb49788903c43523cdf45
VillanuevaCruzAntonio <mrvillantocruz@gmail.com>
2018-03-24 14:59:11 -0600

    Update search.js

--- search.js
+++ search.js
@@ -1,44 +1,12 @@
 function tplawesome(e,t){res=e;for(var n=0;n<t.length;n++){res=res.replace(/\{\{(.*?)\}\}/g,function(e,r){return t[n][r]})}return res}
-/**
-$(function() {
-    $("form").on("submit", function(e) {
-       e.preventDefault();
-       // prepare the request
-       var numResultado = $("#resultados").val();
-       var request = gapi.client.youtube.search.list({
-            part: "snippet",
-            type: "video",
-            q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
-            maxResults: numResultado,
-            order: "viewCount",
-            publishedAfter: "2015-01-01T00:00:00Z"
-       }); 
-       // execute the request
-       request.execute(function(response) {
-          var results = response.result;
-          $("#results").html("");
-          $.each(results.items, function(index, item) {
-            $.get("tpl/item.html", function(data) {
-                $("#results").append(tplawesome(data, [{"title":item.snippet.title, "videoid":item.id.videoId}]));
-            });
-          });
-          resetVideoHeight();
-       });
-    });
-    
-    $(window).on("resize", resetVideoHeight);
-});
-**/
+
 $(function() {
     $("form").on("submit", function(e) {
        e.preventDefault();
            // prepare the request
            var numResultado = $("#resultados").val();
-           var textoBuscar = $("#search").val();
-       if(numResultado == "" || numResultado > 50 || textoBuscar = ""){
-            alert("El campo de busqueda esta vacio  ingresa una cadena a buscar \n" +
-                  "Ó \n"+
-                  "El campo de numero de busqueda esta vacio Ingresa un número menor a 50");
+       if(numResultado == "" || numResultado > 50){
+            alert("El campo de numero de busqueda esta vacio Ingresa un número menor a 50");
        }else{
        
        var request = gapi.client.youtube.search.list({
