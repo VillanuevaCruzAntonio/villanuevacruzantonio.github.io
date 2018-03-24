b07142357123f33280cc595f91c24474c20f6406
VillanuevaCruzAntonio <mrvillantocruz@gmail.com>
2018-03-24 09:16:26 -0600

    Update search.js

--- search.js
+++ search.js
@@ -1,64 +1,18 @@
-/**
+// After the API loads, call a function to enable the search box.
 function handleAPILoaded() {
-  $('#search-button').attr('disable', false);
+  $('#search-button').attr('disabled', false);
 }
 
-function busqueda() {
+// Search for a specified string.
+function search() {
   var q = $('#query').val();
-  var request = gapi.client.youtube.search.list({q: q, part: 'snippet'});
+  var request = gapi.client.youtube.search.list({
+    q: q,
+    part: 'snippet'
+  });
 
   request.execute(function(response) {
     var str = JSON.stringify(response.result);
     $('#search-container').html('<pre>' + str + '</pre>');
   });
-}
-
-function searchByKeyword() {
-  var textoSearch = document.getElementById('query').value;
-  var results = youtube.search.list('id,snippet', {q: textoSearch, maxResults: 25});
-  for(var i in results.items) {
-    var item = results.items[i];
-    Logger.log('[%s] Title: %s', item.id.videoId, item.snippet.title);
-  }
-}
-**/
-function tplawesome(e,t){res=e;for(var n=0;n<t.length;n++){res=res.replace(/\{\{(.*?)\}\}/g,function(e,r){return t[n][r]})}return res}
-
-$(function() {
-    $("form").on("submit", function(e) {
-       e.preventDefault();
-       // prepare the request
-       var request = gapi.client.youtube.search.list({
-            part: "snippet",
-            type: "video",
-            q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
-            maxResults: 3,
-            order: "viewCount",
-            publishedAfter: "2015-01-01T00:00:00Z"
-       }); 
-       // execute the request
-       request.execute(function(response) {
-          var results = response.result;
-          $("#results").html("");
-          $.each(results.items, function(index, item) {
-            $.get("tpl/item.html", function(data) {
-                $("#results").append(tplawesome(data, [{"title":item.snippet.title, "videoid":item.id.videoId}]));
-            });
-          });
-          resetVideoHeight();
-       });
-    });
-    
-    $(window).on("resize", resetVideoHeight);
-});
-
-function resetVideoHeight() {
-    $(".video").css("height", $("#results").width() * 9/16);
-}
-
-function init() {
-    gapi.client.setApiKey("AIzaSyBQGPJQWZ4Rula9OCm10EUkmtlS-6tZrtk");
-    gapi.client.load("youtube", "v3", function() {
-        // yt api is ready
-    });
 }
