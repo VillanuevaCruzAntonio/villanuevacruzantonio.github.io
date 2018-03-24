499571f127b42df14c2d0f79225194016d06d0f8
VillanuevaCruzAntonio <mrvillantocruz@gmail.com>
2018-03-24 14:45:50 -0600

    Update search.js

--- search.js
+++ search.js
@@ -1,5 +1,5 @@
 function tplawesome(e,t){res=e;for(var n=0;n<t.length;n++){res=res.replace(/\{\{(.*?)\}\}/g,function(e,r){return t[n][r]})}return res}
-
+/**
 $(function() {
     $("form").on("submit", function(e) {
        e.preventDefault();
@@ -24,6 +24,41 @@ $(function() {
           });
           resetVideoHeight();
        });
+    });
+    
+    $(window).on("resize", resetVideoHeight);
+});
+**/
+$(function() {
+    $("form").on("submit", function(e) {
+       e.preventDefault();
+           // prepare the request
+           var numResultado = $("#resultados").val();
+       if(numResultado == "" || numResultado > 50){
+            alert("El campo esta vacio Ingresa un número menor a 50");
+       }else{
+       
+       var request = gapi.client.youtube.search.list({
+            part: "snippet",
+            type: "video",
+            q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
+            maxResults: numResultado,
+            order: "viewCount",
+            publishedAfter: "2015-01-01T00:00:00Z"
+       }); 
+       // execute the request
+       request.execute(function(response) {
+          var results = response.result;
+          $("#results").html("");
+          $.each(results.items, function(index, item) {
+            $.get("tpl/item.html", function(data) {
+                $("#results").append(tplawesome(data, [{"title":item.snippet.title, "videoid":item.id.videoId}]));
+            });
+          });
+          resetVideoHeight();
+       });
+       }
+
     });
     
     $(window).on("resize", resetVideoHeight);
